a930a2fbbccdd778733e575686b1e7d111b7dbba
sergio valle hidalgo <ser.valle@alumnos.duoc.cl>
2023-03-28 23:28:05 -0300

    Escena bug re loca con el hedge physics implementado a medias lista
    se corrigieron los pocos errores que quedaban en la pantalla de titulo y se implemento una escena debug por los loles

---
+++ Assets/TitleScreen/Code/StartScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour {
+	public bool loop;
+
+	// Update is called once per frame
+	void Update () {
+		if (UnityEngine.N3DS.GamePad.GetButtonHold(N3dsButton.Start) || Input.GetKey(KeyCode.Space)){
+			SceneManager.LoadScene("Stages/DebugStage");
+		} else if(loop == true){
+			loop = false;
+			SceneManager.LoadScene("TitleScreen/TitleScreen");
+		}
+	}
+}
